3662dd83bfad296afd76a2bcd2985647a646fb22
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-11 11:51:23 -0500

    Update SafeInput.java

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class SafeInput {
 
-    /**
-     * Prompts the user for a non-zero length string.
-     *
-     * @param pipe a Scanner opened to read from System.in
-     * @param prompt prompt for the user
-     * @return a String response that is not zero length
-     */
     public static String getNonZeroLenString(Scanner pipe, String prompt) {
         String retString = "";
         do {
@@ -17,5 +10,19 @@ public class SafeInput {
         } while (retString.length() == 0);
 
         return retString;
+    }
+    public static int getRangedInt(Scanner pipe, String prompt, int min, int max) {
+        int retInt = 0;
+        do {
+            System.out.print("\n" + prompt + " : ");
+            while (!pipe.hasNextInt()) {
+                System.out.print("Invalid input. " + prompt + " : ");
+                pipe.next();
+            }
+            retInt = pipe.nextInt();
+        } while (retInt < min || retInt > max);
+
+        pipe.nextLine(); // clear the input buffer
+        return retInt;
     }
 }
