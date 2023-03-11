b1ea324910af8f239e227877e20ff490cb3ac97a
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-11 12:42:00 -0500

    Update SafeInput.java

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -25,4 +25,32 @@ public class SafeInput {
         pipe.nextLine(); // clear the input buffer
         return retInt;
     }
+public static double getRangedDouble(Scanner scanner, String prompt, double min, double max) {
+        double value = 0;
+        boolean valid = false;
+        do {
+            System.out.print(prompt);
+            if (scanner.hasNextDouble()) {
+                value = scanner.nextDouble();
+                if (value >= min && value <= max) {
+                    valid = true;
+                } else {
+                    System.out.println("Please enter a value between " + min + " and " + max + ".");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next();
+            }
+        } while (!valid);
+        return value;
+    }
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        String response;
+        do {
+            System.out.print(prompt + " (Y/N): ");
+            response = pipe.nextLine().toUpperCase();
+        } while (!response.equals("Y") && !response.equals("N"));
+
+        return response.equals("Y");
+    }
 }
