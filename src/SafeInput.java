496a271ed335bd999485af72382e046a0db1a039
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-11 11:42:01 -0500

    Update SafeInput.java

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class SafeInput {
+import java.util.Scanner;
+
+public class SafeInput {
+
+    /**
+     * Prompts the user for a non-zero length string.
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt) {
+        String retString = "";
+        do {
+            System.out.print("\n" + prompt + " : ");
+            retString = pipe.nextLine();
+        } while (retString.length() == 0);
+
+        return retString;
+    }
 }
